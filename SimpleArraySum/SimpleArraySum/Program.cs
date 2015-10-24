7deb7ae23961e3b1a631a61c90aa06318d3ade2a
alleeclark <allee.clark@bison.howard.edu>
2015-10-24 09:44:46 -0400

    Solution C#

---
+++ SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int N = Convert.ToInt32(Console.ReadLine());
+        string elements = Console.ReadLine();
+        string[] array = elements.Split(' ');
+        int sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            sum += Convert.ToInt32(array[i]);
+        }
+        Console.WriteLine(sum);
+
+    }
+}
