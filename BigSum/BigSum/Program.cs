b285d9c059c08d7377e7537c36d69b6618e62232
alleeclark <allee.clark@bison.howard.edu>
2015-10-24 09:46:30 -0400

    Solution C#

---
+++ BigSum/BigSum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        long N = Convert.ToInt64(Console.ReadLine());
+        string elements = Console.ReadLine();
+        string[] array = elements.Split(' ');
+        long sum = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            sum += Convert.ToInt64(array[i]);
+        }
+        Console.WriteLine(sum);
+    }
+}
